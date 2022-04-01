41af17767b2aa38828f76d243bc43bbdf4002231
Vedran V <drvicko@hotmail.com>
2022-04-01 09:17:13 +0200

    ex01 - String in JS

---
+++ ex01/src/index.js
@@ -0,0 +1,21 @@
+function main () 
+{
+
+    // Only change code below this line
+    
+        var myFirstName = "Vedran";
+        var myLastName = "Vasilj";
+        var myHomeTown = "Tuzla";
+
+    // Only change code above this line
+    
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+
+console.log(main());
+module.exports = main;
